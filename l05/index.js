978e0c7c727163ac83c0b0992d855832ac2365b5
darlopvil <dariolopezvillegas97@gmail.com>
2021-03-21 12:27:45 +0100

    cambios en aire_routes

--- l05/index.js
+++ l05/index.js
@@ -82,10 +82,74 @@ app.get("/info/culturaBASE", (req, res)=>{
     </html>`);
 });
 
+app.get("/info/air_routes", (req, res)=>{
+  res.send(`<!DOCTYPE html>
+  <html>
+  <head>
+  <style>
+  table, th, td {
+    border: 1px solid black;
+  }
+  </style>
+  </head>
+  <body>
+  
+  <h3> <a href=" https://www.fomento.gob.es/BE/?nivel=2&orden=03000000"> Enlace de referencia </a>
+  </h3>
+  
+  <table>
+    <colgroup>
+      <col style="background-color:#f2f2f2">
+    </colgroup>
+    <tr>
+      <th>districts</th>
+      <th>years</th>
+      <th>flights</th>
+      <th>passengers</th>
+      <th>total_goods(kg)</th>
+    </tr>
+    <tr>
+      <td>Madrid</td>
+      <td>2020</td>
+      <td>158405</td>
+      <td>17071089</td>
+      <td>401133380</td>
+    </tr>
+    <tr>
+      <td>Cataluña</td>
+      <td>2017</td>
+      <td>320456</td>
+      <td>47262688</td>
+      <td>156105304</td>
+    </tr>
 
-app.get("/info/air_routes", (req,res) => {
-    res.send("<html><head><title>Air Routes by darlopvil</title><h1>https://www.fomento.gob.es/BE/?nivel=2&orden=03000000</h1></head><body> <tr><th>districts</th> <th>years</th><th>flights</th><th>passengers</th><th>total_goods(kg)</th>    </tr></body></html>")
+    <tr>
+      <td>Andalucía</td>
+      <td>2020</td>
+      <td>78777</td>
+      <td>7441585</td>
+      <td>10561907</td>
+    </tr>
 
+    <tr>
+      <td>Andalucía</td>
+      <td>2019</td>
+      <td>194370</td>
+      <td>27332163</td>
+      <td>12971632</td>
+    </tr>
+
+    <tr>
+      <td>Madrid</td>
+      <td>2019</td>
+      <td>417958</td>
+      <td>61703370</td>
+      <td>558566726</td>
+    </tr>
+  </table>
+  
+  </body>
+  </html>`);
 });
 
 app.listen(port, () => {
