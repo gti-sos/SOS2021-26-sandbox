106d942eb0f4348b8e617f352f03dc9acab5b6f7
darlopvil <dariolopezvillegas97@gmail.com>
2021-03-21 09:25:44 +0100

    prueba heroku l05

--- l05/index.js
+++ l05/index.js
@@ -1,19 +1,17 @@
-console.log("Heyyyyyyyyyy");
 
 var express = require("express");
-var path = require("path");
+//var path = require("path");
 
 var app = express();
 
-var port = 8000;
+var port = (process.env.PORT || 10000);
 
-//app.use("/", express.static("./l05/public"));
+//app.use("/", express.static(path.join(__dirname, "/l05/public")));
 
-app.use("/", express.static(path.join(__dirname, "/l05/public")));
-app.get("/", (req,res) => {
-    res.send("<html><body><h1>Hello from this tiny server</h1></body></html>");
+app.get("/info/air_routes", (req,res) => {
+    res.send("<html><head><title>Air Routes by darlopvil</title><h1>https://www.fomento.gob.es/BE/?nivel=2&orden=03000000</h1></head><body> <tr><th>districts</th> <th>years</th><th>flights</th><th>passengers</th><th>total_goods(kg)</th>    </tr></body></html>")
 });
 
 app.listen(port, () => {
-    console.log("Dentro callback- Tamo ready en puerto " +port);
+    console.log("Dentro callback- Server ready on port " +port);
 });
