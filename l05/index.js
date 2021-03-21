0aafa986eb52d7081a4c9c859df87f7b67ae7d0d
josemarimaya <josemarimaya@gmail.com>
2021-03-21 11:06:24 +0100

    borrado de mierda

--- l05/index.js
+++ l05/index.js
@@ -7,7 +7,7 @@ var app = express();
 var port = (process.env.PORT || 10000);
 
 app.use("/", express.static(path.join(__dirname, "/l05/public")));
-<<<<<<< HEAD
+
 /*app.get("/", (req,res) => {
     res.send("<html><body><h1>Hello from this tiny server</h1></body></html>");
 });*/
