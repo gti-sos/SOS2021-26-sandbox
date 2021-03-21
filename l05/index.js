0b1d354806face962b35b80ecec93790337e6dd4
darlopvil <dariolopezvillegas97@gmail.com>
2021-03-21 10:51:27 +0100

    cambios

--- l05/index.js
+++ l05/index.js
@@ -1,12 +1,12 @@
 
 var express = require("express");
-//var path = require("path");
+var path = require("path");
 
 var app = express();
 
 var port = (process.env.PORT || 10000);
 
-//app.use("/", express.static(path.join(__dirname, "/l05/public")));
+app.use("/", express.static(path.join(__dirname, "/l05/public")));
 
 app.get("/info/air_routes", (req,res) => {
     res.send("<html><head><title>Air Routes by darlopvil</title><h1>https://www.fomento.gob.es/BE/?nivel=2&orden=03000000</h1></head><body> <tr><th>districts</th> <th>years</th><th>flights</th><th>passengers</th><th>total_goods(kg)</th>    </tr></body></html>")
