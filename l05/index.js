6bb4f8744955dca0f9e750923ff19cf5d1ec7142
josemarimaya <josemarimaya@gmail.com>
2021-03-21 11:05:33 +0100

    allá va

--- l05/index.js
+++ l05/index.js
@@ -49,12 +49,13 @@ app.get("/info/culturaBASE", (req, res)=>{
     </table>
     
     </body>
-    </html>`)
-=======
+    </html>`);
+});
+
 
 app.get("/info/air_routes", (req,res) => {
     res.send("<html><head><title>Air Routes by darlopvil</title><h1>https://www.fomento.gob.es/BE/?nivel=2&orden=03000000</h1></head><body> <tr><th>districts</th> <th>years</th><th>flights</th><th>passengers</th><th>total_goods(kg)</th>    </tr></body></html>")
->>>>>>> 0b1d354806face962b35b80ecec93790337e6dd4
+
 });
 
 app.listen(port, () => {
