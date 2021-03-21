928902daef1676380c2abdca3871094827816ddb
josemarimaya <josemarimaya@gmail.com>
2021-03-21 11:01:40 +0100

    tablitas con html jeje

--- l05/index.js
+++ l05/index.js
@@ -10,8 +10,48 @@ var port = 8000;
 //app.use("/", express.static("./l05/public"));
 
 app.use("/", express.static(path.join(__dirname, "/l05/public")));
-app.get("/", (req,res) => {
+/*app.get("/", (req,res) => {
     res.send("<html><body><h1>Hello from this tiny server</h1></body></html>");
+});*/
+
+app.get("/info/culturaBASE", (req, res)=>{
+    res.send(`<!DOCTYPE html>
+    <html>
+    <head>
+    <style>
+    table, th, td {
+      border: 1px solid black;
+    }
+    </style>
+    </head>
+    <body>
+    
+    <h1>The col element</h1>
+    
+    <table>
+      <colgroup>
+        <col span="2" style="background-color:red">
+        <col style="background-color:yellow">
+      </colgroup>
+      <tr>
+        <th>ISBN</th>
+        <th>Title</th>
+        <th>Price</th>
+      </tr>
+      <tr>
+        <td>3476896</td>
+        <td>My first HTML</td>
+        <td>$53</td>
+      </tr>
+      <tr>
+        <td>5869207</td>
+        <td>My first CSS</td>
+        <td>$49</td>
+      </tr>
+    </table>
+    
+    </body>
+    </html>`)
 });
 
 app.listen(port, () => {
