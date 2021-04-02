860325e6e30317eeb5df137b09e08756843a4966
darlopvil <dariolopezvillegas97@gmail.com>
2021-04-02 12:20:23 +0200

    darlopvil v2 added

--- l06/index.js
+++ l06/index.js
@@ -29,10 +29,33 @@ var air_routes=[
 
 ];
 
+// GET GENERAL
 app.get(BASE_API_PATH + "/air-routes", (req,res)=>{
 
     res.send(JSON.stringify(air_routes, null, 2));
+    res.sendStatus(200);
 });
+
+//GET POR RECURSO
+app.get(BASE_API_PATH + "/air-routes/Madrid/2020", (req,res)=>{
+    
+    for(var i=0; i<air_routes.length; i++) {
+        if(air_routes[i].district=="Madrid"){
+            res.send(JSON.stringify(air_routes[i], null, 2));
+        }
+    }
+    res.sendStatus(200);
+});
+
+// POST GENERAL
+app.post(BASE_API_PATH + "/air_routes", (req,res)=>{
+
+    var newAirRoute = req.body;
+    console.log(`new air route to be added: <${JSON.stringify(newAirRoute,null,2)}>`);
+    air_routes.push(newAirRoute);
+    res.sendStatus(201);
+});
+
 
 app.listen(PORT,()=>{
     console.log(`Server ready at ${PORT}!`);
